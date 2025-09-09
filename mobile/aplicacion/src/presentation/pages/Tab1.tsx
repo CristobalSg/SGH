53cc781ff6a787f55543b9a5ca20372b9115687e
platanob <bcarrasco2022@alu.uct.cl>
2025-09-09 11:39:55 -0300

    cambio de rama

--- mobile/aplicacion/src/presentation/pages/Tab1.tsx
+++ mobile/aplicacion/src/presentation/pages/Tab1.tsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonDatetime } from '@ionic/react';
-=======
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import LoginForm from '../components/LoginForm/LoginForm';
->>>>>>> 83c9c309f672a0e659d6ecb99450568a809dc5de
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -11,17 +6,12 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-<<<<<<< HEAD
           <IonTitle>Inicio</IonTitle>
-=======
-          <IonTitle>Login</IonTitle>
->>>>>>> 83c9c309f672a0e659d6ecb99450568a809dc5de
         </IonToolbar>
       </IonHeader>
       
       <IonContent fullscreen className="ion-padding">
         <IonHeader collapse="condense">
-<<<<<<< HEAD
           <IonTitle size="large">Calendario</IonTitle>
         </IonHeader>
 
@@ -32,18 +22,6 @@ const Tab1: React.FC = () => {
             showDefaultButtons={true}
           ></IonDatetime>
         </div>
-=======
-          <IonToolbar>
-            <IonTitle size="large">Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        {/* Aquí va el login */}
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
-          <LoginForm />
-        </div>
-
->>>>>>> 83c9c309f672a0e659d6ecb99450568a809dc5de
       </IonContent>
     </IonPage>
   );
