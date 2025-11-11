f36f6cc86eaa7b46f5ae792669c1c2baf66c8eb5
omellado-gh <omellado2022@alu.uct.cl>
2025-11-10 23:14:24 -0300

    fix: configuración de las variables de entorno en kubernetes

--- deploy/kubernetes/secrets/make-secrets.sh
+++ deploy/kubernetes/secrets/make-secrets.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+admin_password="$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9!-/' | head -c 32)Aa2*"
+
+cat <<EOF > admin-secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: admin-secret
+  namespace: sgh
+type: kubernetes.io/basic-auth
+stringData:
+  username: admin
+  password: $admin_password
+EOF
+
 postgres_password=$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 32)
 
-cat <<EOF >> postgres-secret.yaml
+cat <<EOF > postgres-secret.yaml
 apiVersion: v1
 kind: Secret
 metadata:
@@ -10,7 +24,7 @@ metadata:
   namespace: sgh
 type: kubernetes.io/basic-auth
 stringData:
-  username: sgh
+  username: sgh_admin
   password: $postgres_password
 ---
 apiVersion: v1
@@ -19,12 +33,13 @@ metadata:
   name: postgres-url-secret
   namespace: sgh
 stringData:
-  url: postgresql://sgh:$postgres_password@postgres-cluster-rw.sgh.svc/sgh_production
+  url: postgresql://sgh_admin:$postgres_password@postgres-cluster-rw.sgh.svc/sgh_production
 EOF
 
 jwt_secret=$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 64)
+refresh_secret=$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 64)
 
-cat <<EOF >> jwt-secret.yaml
+cat <<EOF > jwt-secret.yaml
 apiVersion: v1
 kind: Secret
 metadata:
@@ -32,4 +47,6 @@ metadata:
   namespace: sgh
 stringData:
   secret: $jwt_secret
+  refresh: $refresh_secret
 EOF
+
